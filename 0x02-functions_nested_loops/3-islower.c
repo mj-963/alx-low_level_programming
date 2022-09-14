2733dfab260e405a771799cd86c02f3af8ca5cf1
Marcus Jacob <mjportfoliomail@gmail.com>
2022-09-13 19:37:39 -0700

    dhihdifne fhiehffef

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase letters
- * Description: checks for lowercase letters
  * @c: character for checking
- * return: 0 or 1
+ * Return: 0 or 1
  */
 int _islower(int c)
 {
