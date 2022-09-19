38a748220a9926f0e7abfe4ded00375a5341b737
Marcus Jacob <mjportfoliomail@gmail.com>
2022-09-19 09:41:53 -0700

    prints a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: inputs string.
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] != '\0')
+		{
+			_putchar(str[count]);
+			count++;
+		} else
+		{
+			count = -1;
+			_putchar('\n');
+		}
+	}
+}
