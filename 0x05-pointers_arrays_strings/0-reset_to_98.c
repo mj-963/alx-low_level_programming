d66fa45e12828125436018ba787d5902d916112f
Marcus Jacob <mjportfoliomail@gmail.com>
2022-09-19 08:30:27 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <main.h>
 /**
- * reset_to_9 - takes reset its value to 98.
- * @n - input int.
+ * reset_to_9:  takes reset its value to 98.
+ * @n: input int.
  * return: void.
  */
 void reset_to_98(int *n)
