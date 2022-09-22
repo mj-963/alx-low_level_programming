385904101d7a8a7a270a59ca88edc19806ccc79f
Marcus Jacob <mjportfoliomail@gmail.com>
2022-09-22 06:23:03 -0700

    concatenates strings at index n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: represent dest string.
+ * @src: represents src string.
+ * @n: Amount of byte to use.
+ * Return: dest.
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int size1 = 0;
+	int size2 = 0;
+
+	while (*(dest + size1) != '\0')
+		size1++;
+
+	while (size2 < n)
+	{
+		*(dest + size1) = *(src + size2);
+		if (*(src + size2) == '\0')
+			break;
+		size1++;
+		size2++;
+	}
+
+	return (dest);
+}
