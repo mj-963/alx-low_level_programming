9af54b14b1b4dc32f70d316b3eccf4359eb3880c
Marcus Jacob <mjportfoliomail@gmail.com>
2022-09-28 07:26:05 -0700

    $cmes

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length a string.
+ * @s: string to count.
+ * Returns: length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
