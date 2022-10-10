e110869e9b572dd73522744f9977dd854918e1c3
Marcus Jacob <mjportfoliomail@gmail.com>
2022-10-10 13:46:39 -0700

    done

---
+++ 0x03-debugging/sayhi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this prints out hello world.
+ * Description: this is the main function.
+ * Return: 0
+ */
+
+int main(void)
+{
+
+printf("Hello world");
+
+return (0);
+}
